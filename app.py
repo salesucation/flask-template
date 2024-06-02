7c936aec5a096d5e01662f19140a20a8bb17caee
Richard Hildred <rhildred@gmail.com>
2024-06-02 22:26:39 +0000

    add article

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ pages = FlatPages(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html");
+    page = pages.get_or_404("index")
+    # Articles are pages with a publication date
+    articles = (p for p in pages if 'published' in p.meta)
+    # Show the 10 most recent articles, most recent first.
+    latest = sorted(articles, reverse=True,
+                    key=lambda p: p.meta['published'])
+    return render_template('index.html', articles=latest[:10], page=page)
 
 @app.route('/<path:path>')
 def page(path):
