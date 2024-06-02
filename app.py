def704ba5bd95d0574a983208458498c5f98f7b8
Richard Hildred <rhildred@gmail.com>
2024-06-02 20:55:29 +0000

    add flatpages

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
 
 app = Flask(__name__)
+pages = FlatPages(app)
 
 @app.route("/")
 def index():
     return render_template("index.html");
+
+@app.route('/<path:path>')
+def page(path):
+    page = pages.get_or_404(path)
+    template = page.meta.get('template', 'article.html')
+    return render_template(template, page=page)
