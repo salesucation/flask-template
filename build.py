f4011b6baee855ae46272a8f311c462cee6208dd
Richard Hildred <rhildred@gmail.com>
2024-06-03 04:48:15 +0000

    kind of sort of working

--- build.py
+++ build.py
@@ -1,7 +1,19 @@
 from flask_frozen import Freezer
+from flask import url_for, render_template_string
 from app import app
+from flask_flatpages import FlatPages
+import os
 
 freezer = Freezer(app)
+pages = FlatPages(app)
 
 if __name__ == '__main__':
-    freezer.freeze()
+    with app.app_context():
+    #freezer.freeze()
+        for page in pages:
+            template = page.meta.get('template', 'article.html')
+            sPage = render_template_string(template, page=page)
+            os.makedirs(f"{os.getcwd()}/build/{page.path}")
+            f = open(f"{os.getcwd()}/build/{page.path}/index.html", "a")
+            f.write(sPage)
+            f.close()
