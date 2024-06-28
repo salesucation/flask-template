ce2400c9a97b05fd331133ad8d3edd7819ce7770
Richard Hildred <rhildred@gmail.com>
2024-06-28 16:28:04 +0000

    add a static graph

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
+import pandas as pd
+df = pd.read_csv('https://anvil.works/blog/img/plotting-in-python/uk-election-results.csv')
+import pygal
+from pygal.style import Style
+
+custom_style = Style(
+    colors=('#0343df', '#e50000', '#ffff14', '#929591'),
+    font_family='Roboto,Helvetica,Arial,sans-serif',
+    background='transparent',
+    label_font_size=14,
+)
+
+c = pygal.Bar(
+    title="UK Election Results",
+    y_title='Seats',
+    width=800,
+    x_label_rotation=270,
+)
+c.add('Conservative', df['conservative'])
+c.add('Labour', df['labour'])
+c.add('Liberal', df['liberal'])
+c.add('Others', df['others'])
+
+c.x_labels = df['year']
+c.render_to_file('static/pygal.svg')
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
