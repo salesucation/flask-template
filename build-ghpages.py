0dda6ba86070fa6929ef89ae1578fe0066fc5cee
salesucation <rhildred@salesucation.ca>
2024-07-01 11:56:51 -0400

    Delete build-ghpages.py ... not needed

--- build-ghpages.py
+++
@@ -1,8 +0,0 @@
-from flask_frozen import Freezer
-from app import app
-
-app.config.FREEZER_BASE_URL = "https://salesucation.github.io"
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
