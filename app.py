ef24a5d0bc79aa8eb93603ca3d15046069756fc8
Richard Hildred <rhildred@gmail.com>
2024-07-01 03:20:13 +0000

    work with dark theme

--- app.py
+++ app.py
@@ -1,30 +1,8 @@
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
 import pandas as pd
-df = pd.read_csv('https://anvil.works/blog/img/plotting-in-python/uk-election-results.csv')
 import pygal
 from pygal.style import Style
-
-custom_style = Style(
-    colors=('#0343df', '#e50000', '#ffff14', '#929591'),
-    font_family='Roboto,Helvetica,Arial,sans-serif',
-    background='transparent',
-    label_font_size=14,
-)
-
-c = pygal.Bar(
-    title="UK Election Results",
-    y_title='Seats',
-    width=800,
-    x_label_rotation=270,
-)
-c.add('Conservative', df['conservative'])
-c.add('Labour', df['labour'])
-c.add('Liberal', df['liberal'])
-c.add('Others', df['others'])
-
-c.x_labels = df['year']
-c.render_to_file('static/pygal.svg')
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -39,6 +17,36 @@ def index():
     latest = sorted(articles, reverse=True,
                     key=lambda p: p.meta['published'])
     return render_template('index.html', articles=latest[:10], page=page)
+
+@app.route("/status")
+def status():
+    page = pages.get_or_404("status")
+    df = pd.read_csv('https://anvil.works/blog/img/plotting-in-python/uk-election-results.csv')
+
+    custom_style = Style(
+        colors=('#0343df', '#e50000', '#ffff14', '#929591'),
+        font_family='Roboto,Helvetica,Arial,sans-serif',
+        background='transparent',
+        plot_background='transparent',
+        label_font_size=14,
+    )
+
+    c = pygal.Bar(
+        title="UK Election Results",
+        style=custom_style,
+        y_title='Seats',
+        x_label_rotation=270,
+        no_prefix=True
+    )
+    c.add('Conservative', df['conservative'])
+    c.add('Labour', df['labour'])
+    c.add('Liberal', df['liberal'])
+    c.add('Others', df['others'])
+
+    c.x_labels = df['year']
+    # c.render_to_file('static/pygal.svg')
+    graph_data = c.render(is_unicode=True)
+    return render_template("status_page.html", page=page, graph_data=graph_data)
 
 @app.route('/<path:path>')
 def page(path):
