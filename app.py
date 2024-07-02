efbd5e3eb473cfd74dc7427734a764f794d52347
salesucation <rhildred@salesucation.ca>
2024-07-02 00:25:51 +0000

    french and english

--- app.py
+++ app.py
@@ -16,22 +16,18 @@ def getLang():
     else:
         return app.config["DEFAULT_LANG"]
 
-
-
-
-@app.route("/")
-def index():
-    lang = getLang()
-    page = pages.get_or_404(f"{lang}/index")
-    # Articles are pages with a publication date
-    articles = (p for p in pages if 'published' in p.meta and lang in p.path)
-    # Show the 10 most recent articles, most recent first.
-    latest = sorted(articles, reverse=True,
-                    key=lambda p: p.meta['published'])
-    return render_template(f'{lang}/index.html', articles=latest[:10], page=page)
-
 @app.route('/<path:path>')
 def page(path):
+    aParts = path.split("/")
+    lang = aParts[0]
     page = pages.get_or_404(path)
-    template = page.meta.get('template', f'{getLang()}/article.html')
-    return render_template(template, page=page)
+    template = page.meta.get('template', f'{lang}/article.html')
+    if path.endswith("index"):
+        # Articles are pages with a publication date
+        articles = (p for p in pages if 'published' in p.meta and lang in p.path)
+        # Show the 10 most recent articles, most recent first.
+        latest = sorted(articles, reverse=True,
+                        key=lambda p: p.meta['published'])
+        return render_template(template, articles=latest[:10], page=page)
+    else:
+        return render_template(template, page=page)
--- build.py
+++ build.py
@@ -6,11 +6,13 @@ client = app.test_client();
 
 def load(path):
     res = client.get(path)
-    if path != "/":
+    if path.endswith("index"):
+        path = path.replace("/index", "")
+    try:
         os.makedirs(f"{os.getcwd()}/build/{path}")
-        f = open(f"{os.getcwd()}/build/{path}/index.html", "wb")
-    else:
-        f = open(f"{os.getcwd()}/build/index.html", "wb")
+    except:
+        pass
+    f = open(f"{os.getcwd()}/build/{path}/index.html", "wb")
     f.write(res.data)  
     f.close()
 
@@ -19,4 +21,3 @@ if os.path.exists(f"{os.getcwd()}/build"):
 shutil.copytree(f"{os.getcwd()}/static", f"{os.getcwd()}/build/static")
 for page in pages:
     load(page.path)
-load("/")
